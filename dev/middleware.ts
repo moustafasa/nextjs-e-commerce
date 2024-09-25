e40d1bd9dab6f97feb30c250c3aca34270f7d8d7
moustafa saad <moustafasaad954@gmail.com>
2024-09-25 10:10:45 +0300

    resolving the deploying problems

--- dev/middleware.ts
+++ dev/middleware.ts
@@ -8,4 +8,5 @@ export default auth;
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  unstable_allowDynamic: ["/config/dbConnect.ts"],
 };
