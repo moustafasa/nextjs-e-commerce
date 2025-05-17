ca978f05425ce25405a07dc8168b0af3742183f0
moustafa ramadan moustafa saad 6191176 <moustafasaad954@gmail.com>
2025-05-17 15:15:12 +0300

    remove dashboard loading

--- dev/app/(UsersRoutes)/page.tsx
+++ dev/app/(UsersRoutes)/page.tsx
@@ -8,33 +8,32 @@ import LottieAnimeSk from "./_components/LottieAnimeSk";
 const LottieAnime = lazy(() => import("./_components/HomePageAnime"));
 
 export default function Home() {
-  return <div></div>;
-  // return (
-  //   <div className="flex-col-reverse lg:flex-row px-7 dark:text-white text-black-bg flex gap-3 items-center mt-3  container mx-auto  justify-center lg:px-7">
-  //     <div className="lg:max-w-[600px] lg:w-[45%] text-center lg:text-left">
-  //       <h1 className="text-4xl font-bold capitalize mb-8">
-  //         welcome to moustafa clothes shopping
-  //       </h1>
-  //       <p className="text-balance leading-relaxed dark:text-gray-icons  text-lg">
-  //         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
-  //         mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
-  //         mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
-  //         tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non
-  //         suscipit magna interdum eu. Curabitur pellentesque nibh nibh,
-  //       </p>
-  //       <Link
-  //         href={"/shop-now"}
-  //         className="flex items-center justify-center lg:justify-start gap-3 mt-10 text-3xl lg:ms-20"
-  //       >
-  //         Shop Now <FaArrowRight />
-  //       </Link>
-  //     </div>
-  //     <div className="lg:w-[45%] flex justify-center items-center">
-  //       {/* <LottieAnimeSk /> */}
-  //       <Suspense fallback={<LottieAnimeSk />}>
-  //         <LottieAnime />
-  //       </Suspense>
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div className="flex-col-reverse lg:flex-row px-7 dark:text-white text-black-bg flex gap-3 items-center mt-3  container mx-auto  justify-center lg:px-7">
+      <div className="lg:max-w-[600px] lg:w-[45%] text-center lg:text-left">
+        <h1 className="text-4xl font-bold capitalize mb-8">
+          welcome to moustafa clothes shopping
+        </h1>
+        <p className="text-balance leading-relaxed dark:text-gray-icons  text-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
+          mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
+          mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
+          tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non
+          suscipit magna interdum eu. Curabitur pellentesque nibh nibh,
+        </p>
+        <Link
+          href={"/shop-now"}
+          className="flex items-center justify-center lg:justify-start gap-3 mt-10 text-3xl lg:ms-20"
+        >
+          Shop Now <FaArrowRight />
+        </Link>
+      </div>
+      <div className="lg:w-[45%] flex justify-center items-center">
+        <LottieAnimeSk />
+        {/* <Suspense fallback={<LottieAnimeSk />}>
+          <LottieAnime />
+        </Suspense> */}
+      </div>
+    </div>
+  );
 }
